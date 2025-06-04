cbc73ff132f86b2cfe9fe4f6204fce55fa8e30ee
omar <multitartanga@gmail.com>
2025-06-04 11:36:44 +0200

    [IMP] improve sql script

--- scripts/SQL_Script.sql
+++ scripts/SQL_Script.sql
@@ -1,23 +1,24 @@
+
 CREATE TABLE Comunidad (
-    ID_Comunidad INT PRIMARY KEY,
+    ID_Comunidad INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Nombre VARCHAR(100)
 );
 
 CREATE TABLE Provincia (
-    ID_Provincia INT PRIMARY KEY,
+    ID_Provincia INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     ID_Comunidad INT,
     Nombre VARCHAR(100),
     FOREIGN KEY (ID_Comunidad) REFERENCES Comunidad(ID_Comunidad)
 );
 
 CREATE TABLE Partido (
-    ID_Partido INT PRIMARY KEY,
+    ID_Partido INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Nombre VARCHAR(100),
     Siglas VARCHAR(20)
 );
 
 CREATE TABLE Elecciones (
-    ID_Elecciones INT PRIMARY KEY,
+    ID_Elecciones INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Anio INT,
     Tipo VARCHAR(50)
 );
@@ -36,20 +37,31 @@ CREATE TABLE Resultados (
 CREATE TABLE Diputados (
     ID_Elecciones INT,
     ID_Partido INT,
-    ID_Provincia INT,
     Diputados INT,
-    PRIMARY KEY (ID_Elecciones, ID_Partido, ID_Provincia),
+    PRIMARY KEY (ID_Elecciones, ID_Partido),
     FOREIGN KEY (ID_Elecciones) REFERENCES Elecciones(ID_Elecciones),
-    FOREIGN KEY (ID_Partido) REFERENCES Partido(ID_Partido),
-    FOREIGN KEY (ID_Provincia) REFERENCES Provincia(ID_Provincia)
+    FOREIGN KEY (ID_Partido) REFERENCES Partido(ID_Partido)
 );
 
 CREATE TABLE Votantes (
     ID_Elecciones INT,
     ID_Provincia INT,
     Poblacion INT,
+    Numero_Mesas INT,
+    Censo_Sin_CERA INT,
+    Censo_CERA INT,
+    Total_Censo_Electoral INT,
+    Solicitudes_CERA_Aceptadas INT,
+	Total_Votantes_CER INT,
+	Total_Votantes_CERA INT,
+    Total_Votantes INT,
+    Votos_Validos INT,
+    Votos_Candidaturas INT,
+    Votos_Blanco INT,
     Votos_Nulos INT,
     PRIMARY KEY (ID_Elecciones, ID_Provincia),
     FOREIGN KEY (ID_Elecciones) REFERENCES Elecciones(ID_Elecciones),
     FOREIGN KEY (ID_Provincia) REFERENCES Provincia(ID_Provincia)
 );
+
+
